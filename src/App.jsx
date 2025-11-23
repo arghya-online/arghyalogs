4ba3f7a661fa32c79e9662ed2fdedb8fa48a71c1
Arghya <168770837+arghya-online@users.noreply.github.com>
2025-11-23 15:52:57 +0530

    Fix import path for Layout component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { ThemeProvider } from './context/ThemeContext';
-import Layout from './components/Layout/layout';
+import Layout from './components/layout/Layout';
 import Home from './pages/Home';
 import BlogPage from './pages/BlogPage';
 
