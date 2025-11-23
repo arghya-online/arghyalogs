f63b67be737299f7385d7dd499f51692d11b3065
arghya-online <arghya0nline555@gmail.com>
2025-11-23 20:41:34 +0530

    fgh

--- src/blogs/index.js
+++ src/blogs/index.js
@@ -1,10 +1,12 @@
 import MathIntro, { meta as mathMeta } from './mathematics/MathIntro';
+import BayesTheoremBlog, {meta as bayesMeta} from './mathematics/BayesTheoremBlog';
 import JamesWebbTelescope, { meta as jwstMeta } from './astronomy/JamesWebbTelescope';
 import RoboticsIntro, { meta as roboticsMeta } from './robotics/RoboticsIntro';
 import { getReadingTime } from '../utils/readingTime';
 
 const posts = [
     { ...mathMeta, Component: MathIntro, readingTime: getReadingTime(mathMeta.plainText) },
+    { ...bayesMeta, Component: BayesTheoremBlog, readingTime: getReadingTime(bayesMeta.plainText) },
     { ...jwstMeta, Component: JamesWebbTelescope, readingTime: getReadingTime(jwstMeta.plainText) },
     { ...roboticsMeta, Component: RoboticsIntro, readingTime: getReadingTime(roboticsMeta.plainText) }
 ];
